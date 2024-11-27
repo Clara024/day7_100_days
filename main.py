555c159456495f15fbbf6362a6347ae73ce23674
Izundu Clara Chinecherem <ci.chinecherem@stu.unizik.edu.ng>
2024-11-27 20:01:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+movie= input("What is your favorite tragic movie?")
+if movie== "Titanic":
+  print("That movie was soo sad!")
+  character=input("Who was your favorite character?")
+  if character =="Jack":
+    print("He was a hero!")
+  if character =="Rose":
+    print("She was a brave lady!")
+  else:
+    print("It was really tragic!")
+elif movie== "Me before you":
+  print("I love that movie!")
+  character=input("Who was your favorite character?")
+  if character =="Sam":
+    print("He was a good man!")
+  if character =="Emilia":
+    print("She was a crazy one!")
+else:
+  print("I bet its sad like the rest")
